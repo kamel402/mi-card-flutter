7a1331016f89530735105266a6e99ee312670f69
kamel402 <kamel1419@gmail.com>
2022-03-04 04:20:28 +0300

    challange commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,53 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                color: Colors.red,
+                width: 100.0,
+              ),
+              SizedBox(
+                width: 45.0,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    color: Colors.yellow,
+                    width: 100.0,
+                    height: 100.0,
+                  ),
+                  Container(
+                    color: Color.fromRGBO(154, 205, 50, 0.5),
+                    width: 100.0,
+                    height: 100.0,
+                  ),
+                ],
+              ),
+              SizedBox(
+                width: 45.0,
+              ),
+              Container(
+                color: Colors.blue,
+                width: 100.0,
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
